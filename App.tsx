0dd4a35749ba12da181c81e61b8d4af1d58bdb08
Iris <mac@macs-MacBook-Pro.local>
2022-06-12 23:22:04 -0700

    setup environment

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+import HomeScreen from "./screens/home";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  Theme,
+} from "@react-navigation/native";
+import { Text } from "react-native";
+const theme: Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  },
+};
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [loading] = useFonts({});
+
+  if (!loading) return <Text>Font Not Loaded</Text>;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        initialRouteName="Home"
+        defaultScreenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen component={HomeScreen} name="Home" />
+        <Stack.Screen component={HomeScreen} name="book" />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text } from "react-native";
+
+function Home() {
+  return (
+    <Text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quisquam
+      blanditiis quidem error cumque, eligendi qui odit magni nam harum alias
+      exercitationem praesentium placeat debitis dolor, totam velit aperiam
+      veritatis!
+    </Text>
+  );
+}
+
+export default Home;
